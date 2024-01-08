a21226a4c0c6eed9a543d1458254e6d1e66b34df
Luishenriquee <iluishenriquebr@gmail.com>
2024-01-07 22:08:07 -0300

    Creation of migration and configuration in category and product properties using a simple API that configures the mapping between model classes and the database schema

--- VirtualShop.ProductApi/Context/AppDbContext.cs
+++ VirtualShop.ProductApi/Context/AppDbContext.cs
@@ -5,12 +5,53 @@ namespace VirtualShop.ProductApi.Context;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-            
-    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<Category> Categories {  get; set; }
     public DbSet<Product> Products { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //Category
+        modelBuilder.Entity<Category>().HasKey(c => c.CategoryId);
+
+        modelBuilder.Entity<Category>().Property(c => c.Name)
+                                       .HasMaxLength(100)
+                                       .IsRequired();
+
+        //Product
+        modelBuilder.Entity<Product>().Property(p => p.Name)
+                                      .HasMaxLength(100)
+                                      .IsRequired();
+
+        modelBuilder.Entity<Product>().Property(p => p.Description)
+                                      .HasMaxLength(255)
+                                      .IsRequired();
+
+        modelBuilder.Entity<Product>().Property(p => p.ImageURL)
+                                      .HasMaxLength(255)
+                                      .IsRequired();
+
+        modelBuilder.Entity<Product>().Property(p => p.Price)
+                                      .HasPrecision(12, 2);
+
+        modelBuilder.Entity<Category>().HasMany(c => c.Products)
+                                       .WithOne(c => c.Category)
+                                       .IsRequired()
+                                       .OnDelete(DeleteBehavior.Cascade);
+
+        //Popular tabela
+        modelBuilder.Entity<Category>().HasData(
+            new Category
+            {
+                CategoryId = 1,
+                Name = "Material Escolar"
+            },
+            new Category
+            {
+                CategoryId = 2,
+                Name = "Acessórios"
+            }
+        );
+    }
 }
