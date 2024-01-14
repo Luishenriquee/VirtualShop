5de4039f79cee7278a3c71e599cd96e9c2f80faf
Luishenriquee <iluishenriquebr@gmail.com>
2024-01-14 16:45:13 -0300

    Ajustes

--- VirtualShop.ProductApi/DTOs/ProductDTO.cs
+++ VirtualShop.ProductApi/DTOs/ProductDTO.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using VirtualShop.ProductApi.Models;
 
 namespace VirtualShop.ProductApi.DTOs;
@@ -24,6 +25,8 @@ public class ProductDTO
     [Range(1, 9999)]
     public long Stock { get; set; }
     public string? ImageURL { get; set; }
+
+    [JsonIgnore]
     public Category? Category { get; set; }
     public int CategoryId { get; set; }
 }
--- VirtualShop.ProductApi/Program.cs
+++ VirtualShop.ProductApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 using VirtualShop.ProductApi.Context;
 using VirtualShop.ProductApi.Repositories;
 using VirtualShop.ProductApi.Services;
@@ -13,32 +14,22 @@ namespace VirtualShop.ProductApi
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
-            //var mySqlConnection = builder.Configuration.GetConnectionString("DataBase");
-
-            //builder.Services.AddDbContext<AppDbContext>(options => 
-            //                 options.UseMySql(mySqlConnection, 
-            //                 ServerVersion.AutoDetect(mySqlConnection)));
-
 
             builder.Services.AddEntityFrameworkSqlServer().AddDbContext<AppDbContext>
             (
                 options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase"))
             );
 
-            //builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
-            //builder.Services.AddScoped<ITarefaRepositorio, TarefaRepositorio>();
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
             builder.Services.AddScoped<IProductRepository, ProductRepository>();
             builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddScoped<ICategoryService, CategoryService>();
-
 
             var app = builder.Build();
 
@@ -50,12 +41,8 @@ namespace VirtualShop.ProductApi
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
