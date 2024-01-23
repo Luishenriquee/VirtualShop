520036736f5fed7c5fff8e48131c36f9152e7daf
Luishenriquee <iluishenriquebr@gmail.com>
2024-01-22 22:25:16 -0300

    Implementação dos métodos para o serviço de Produto

--- VirtualShop.Web/Services/Contracts/IProductService.cs
+++ VirtualShop.Web/Services/Contracts/IProductService.cs
@@ -5,7 +5,7 @@ namespace VirtualShop.Web.Services.Contracts;
 public interface IProductService
 {
     Task<IEnumerable<ProductViewModel>> GetAllProducts();
-    Task<ProductViewModel> FindProductById();
+    Task<ProductViewModel> FindProductById(int id);
     Task<ProductViewModel> CreateProduct(ProductViewModel productVM);
     Task<ProductViewModel> UpdateProduct(ProductViewModel productVM);
     Task<bool> DeleteProductById(int id);
