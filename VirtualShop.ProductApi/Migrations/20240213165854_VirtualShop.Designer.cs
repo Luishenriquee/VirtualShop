9fa45604f960536bf99e8c9842cab554e03749d9
Luishenriquee <iluishenriquebr@gmail.com>
2024-02-13 14:11:49 -0300

    Adição da Migrations

--- VirtualShop.ProductApi/Migrations/20240213165854_VirtualShop.Designer.cs
+++ VirtualShop.ProductApi/Migrations/20240213165854_VirtualShop.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VirtualShop.ProductApi.Context;
 
@@ -10,9 +11,10 @@ using VirtualShop.ProductApi.Context;
 namespace VirtualShop.ProductApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240213165854_VirtualShop")]
+    partial class VirtualShop
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
